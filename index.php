42387e9b48ef572adec78b232a5b04913aa317a5
hectorgrm <hectormx91@hotmail.com>
2025-09-15 14:49:23 -0600

    opcion de usuario
    Opcion de usuario quien ingresa rol recidencia rol Servicio Social

--- index.php
+++ index.php
@@ -3,10 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <h1>Hellow world</h1>
+    <title>Entrada</title>
 </head>
 <body>
-    
+    <h1>Bienvenido a Vinculacion</h1>
+    <div class="Container">
+        <div class="card">
+            <h2>Residencia / Vinculación</h2>
+            <a href="common/login.php?modulo=recidencia" class="btn btn-rec">Ingresar</a>
+        </div>
+
+        <div class="card">
+            <h2>Servicio Social</h2>
+            <a href="common/login.php?modulos=ss" class="btn btn-ss">Ingresar</a>
+        </div>
+    </div>
 </body>
 </html>
