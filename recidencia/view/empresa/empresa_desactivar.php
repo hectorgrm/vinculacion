9cc50d9b4b2c977d08bd4cb5f87f7678c7d726d9
hectorgrm <hectormx91@hotmail.com>
2025-11-17 03:52:28 -0600

    desactivar css rectificado

--- recidencia/view/empresa/empresa_desactivar.php
+++ recidencia/view/empresa/empresa_desactivar.php
@@ -5,7 +5,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <title>Desactivar Empresa · Residencias Profesionales</title>
 
-  <link rel="stylesheet" href="../../assets/stylesrecidencia.css"/>
   <link rel="stylesheet" href="../../assets/css/empresas/empresadelete.css">
 </head>
 
