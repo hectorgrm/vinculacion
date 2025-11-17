218285f8ea5e1a5d6be8fa22b3834f154898bd72
hectorgrm <hectormx91@hotmail.com>
2025-11-17 01:48:55 -0600

    Index.php
      <link rel="stylesheet" href="assets/css/dashboard.css">
      <link rel="stylesheet" href="assets/css/layout/sidebar.css">

--- recidencia/index.php
+++ recidencia/index.php
@@ -17,7 +17,6 @@ if (!is_array($user) || !in_array(strtolower((string) ($user['role'] ?? '')), $a
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Dashboard Residencia</title>
-  <link rel="stylesheet" href="assets/stylesrecidencia.css">
   <link rel="stylesheet" href="assets/css/dashboard.css">
   <link rel="stylesheet" href="assets/css/layout/sidebar.css">
   
